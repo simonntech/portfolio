ac7f46b174208093336a7da16e6896068fd09a9d
SimonTech <contato.simon.tech@gmail.com>
2026-04-15 16:49:14 -0300

    link to github in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-slate-950 text-center py-10 border-t border-slate-950">
       <p className="text-gray-300 text-sm">
-        <span className="text-purple-400 font-bold">© 2026 SIMONN TECH</span> - Todos os direitos reservados
+        <span className="text-purple-400 font-bold">© 2026 <a href="https://github.com/simonntech" target="_blank">SIMONN TECH</a></span> - Todos os direitos reservados
       </p>
     </footer>
   );
