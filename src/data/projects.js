5326d25fef93401223b5982594dfff2a466a6c0d
SimonTech <contato.simon.tech@gmail.com>
2026-04-15 16:45:38 -0300

    feat: update project and service images to use imported assets for better maintainability

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,14 @@
+import lotoGenImg from "../../src/assets/lotoGen.jpg";
+import taxasVisaImg from "../../src/assets/taxasVisa.png";
+import jhoKenPowImg from "../../src/assets/jhokenpow.jpg";
+import blogImg from "../../src/assets/blog.jpg";
+
 const projects = [
   {
     name: "Taxas VISA - Andradina/SP",
     description:
       "Sistema de consulta de valores de taxas para Licenças Sanitárias do Setor de Vigilância Sanitária de Andradina/SP, com interface web para fácil acesso e atualização dos dados, além de uma função para copiar o texto já formatado para gerar a taxa no sistema de emissão de taxas da prefeitura.",
-    imgPath: "../../src/assets/taxasVisa.png",
+    imgPath: taxasVisaImg,
     link: "https://simonntech.github.io/taxasVisa/",
     github: "https://github.com/simonntech/taxasVisa",
     tech: "HTML, CSS, JavaScript e Bootstrap",
@@ -12,7 +17,7 @@ const projects = [
     name: "Blog de Tecnologia",
     description:
       "Blog de tecnologia com notícias sobre tecnologias atuais, inteligência artificial, relatos de projetos e estudos. Construído e gerenciado com Wordpress.",
-    imgPath: "../../src/assets/blog.jpg",
+    imgPath: blogImg,
     link: "https://simonntechnews.rf.gd/",
     github: "#",
     tech: "Wordpress",
@@ -20,7 +25,7 @@ const projects = [
   {
     name: "Loto Gen - Gerador de Números para Loterias",
     description: "Aplicação web para gerar números aleatórios para loterias, com opções de personalização para diferentes tipos de jogos, como Mega-Sena, Quina e Lotofácil. ",
-    imgPath: "../../src/assets/lotoGen.jpg",
+    imgPath: lotoGenImg,
     link: "https://simonntech.github.io/lotoGen/",
     github: "https://github.com/simonntech/lotoGen/",
     tech: "HTML, CSS, JavaScript",
@@ -29,7 +34,7 @@ const projects = [
     name: "Jho Ken Pow - Jogo de Pedra, Papel e Tesoura",
     description:
       "Jogo de Pedra, Papel e Tesoura com interface web interativa, onde os usuários podem jogar contra o computador, sempre com resultados aleatórios. ",
-    imgPath: "../../src/assets/jhokenpow.jpg",
+    imgPath: jhoKenPowImg,
     link: "https://simonntech.github.io/jhoKenPow/",
     github: "https://github.com/simonntech/jhoKenPow/",
     tech: "HTML, CSS, JavaScript",
--- src/data/services.js
+++ src/data/services.js
@@ -1,23 +1,27 @@
+import webSitesImg from "../../src/assets/websites.svg";
+import webServicesImg from "../../src/assets/webservices.svg";
+import updatingImg from "../../src/assets/updating.svg";
+
 const services = [
   {
     title: "Criação de Sites",
     description:
       "Sites institucionais modernos, rápidos e responsivos para empresas. Personalizados para destacar sua marca e atrair clientes.",
     price: "À partir de R$ 399,90.",
-    imgPath: "../../src/assets/websites.svg",
+    imgPath: webSitesImg,
   },
   {
     title: "Sistemas Web",
     description:
       "Desenvolvimento de sistemas personalizados para gestão e automação.",
     price: "Orçamento sob consulta.",
-    imgPath: "../../src/assets/webservices.svg",
+    imgPath: webServicesImg,
   },
   {
     title: "Manutenção",
     description: "Atualizações, melhorias e suporte contínuo para seu sistema.",
     price: "Mensalidade à partir de R$99,90.",
-    imgPath: "../../src/assets/updating.svg",
+    imgPath: updatingImg,
   },
 ];
 
